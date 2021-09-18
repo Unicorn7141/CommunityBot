09c49a4b397a4122ce97799ff73d30d5b7017b2a
Unicorn7141 <harelibr@gmail.com>
2021-09-18 20:54:46 +0300

    Removed user commands

--- src/main/kotlin/CommunityCommands.kt
+++ src/main/kotlin/CommunityCommands.kt
@@ -4,17 +4,12 @@ import com.kotlindiscord.kord.extensions.commands.converters.impl.member
 import com.kotlindiscord.kord.extensions.commands.converters.impl.optionalMember
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.chatCommand
-import com.kotlindiscord.kord.extensions.extensions.publicUserCommand
-import com.kotlindiscord.kord.extensions.interactions.respond
 import com.kotlindiscord.kord.extensions.utils.capitalizeWords
-import com.kotlindiscord.kord.extensions.utils.isNullOrBot
 import dev.kord.common.Color
 import dev.kord.core.behavior.reply
 import dev.kord.rest.Image
 import dev.kord.rest.builder.message.create.allowedMentions
 import dev.kord.rest.builder.message.create.embed
-import org.litote.kmongo.eq
-import org.litote.kmongo.findOne
 import java.text.DecimalFormat
 import java.time.LocalDateTime
 import java.time.ZoneOffset
@@ -46,106 +41,6 @@ class CommunityCommands : Extension() {
 	}
 	
 	override suspend fun setup() {
-		/* User Commands */
-		// Info
-		publicUserCommand {
-			name = "info"
-			
-			action {
-				val target = targetUsers.toList().map {
-					it.asMember(guild?.id ?: error("Invalid Guild"))
-				}.first()
-				var ser = cache[guild?.id?.asString] ?: error("Not a valid guild")
-				cache[ser.id] =
-					database.findOne { Server::id eq ser.id } ?: error("Cannot find any server with that id")
-				ser = cache[ser.id]!!
-				val mem = ser.community.firstOrNull { it.id == target.id.asString }
-				try {
-					if (mem == null) {
-						respond {
-							content = "This user isn't a part of the community (probably a city hall worker)"
-						}
-					} else {
-						respond {
-							embed {
-								thumbnail { url = target.avatar.getUrl(Image.Size.Size2048) }
-								author { name = "ID: ${target.id.asString}" }
-								title = "[INFO] ${target.displayName}"
-								with(mem) {
-									val decimalFormat = DecimalFormat("000")
-									field("Bank", true) { "```\n$money \u235F```" }
-									field("\u200b", true) { "\u200b" }
-									field("Items Count", true) {
-										"```\n${decimalFormat.format(items.size)}```"
-									}
-									field("Item List", true) {
-										"""```css
-                                |${items.joinToString("\n")}${"\u200b"}
-                                |```""".trimMargin()
-									}
-								}
-								color = target.color()
-							}
-						}
-					}
-				} catch (e: Exception) {
-					println("[!!] ERROR: ${e.message}")
-				}
-			}
-		}
-		// Avatar
-		publicUserCommand {
-			name = "avatar"
-			
-			action {
-				respond {
-					allowedMentions()
-					content = targetUsers.toList()[0].avatar.getUrl(Image.Size.Size2048)
-				}
-			}
-		}
-		// Gift 100
-		publicUserCommand {
-			name = "gift 100"
-			
-			action {
-				val ser = cache[guild?.id?.asString] ?: error("Cannot fetch guild: ${guild?.id?.asString}")
-				val gifter = member!!
-				val gifted = targetUsers.toList()[0]
-				val amount = 100
-				val gifterCommunityMember = ser.community.find { gifter.id.asString == it.id }
-											?: error("Cannot find user: ${gifted.id.asString} in guild ${ser.id}")
-				val giftedCommunityMember = ser.community.find { it.id == gifted.id.asString }
-											?: if (gifted.isNullOrBot()) {
-												respond {
-													content =
-														"You can't gift bots and people who are no longer in this server"
-												}
-												error("You may not gift bots")
-											} else null
-				
-				if (gifterCommunityMember.money >= amount) {
-					val giftedIndex = ser.community.indexOf(giftedCommunityMember)
-					val gifterIndex = ser.community.indexOf(gifterCommunityMember)
-					
-					respond {
-						allowedMentions()
-						content = "${gifter.mention} gifted ${gifted.mention} $amount ⍟"
-					}
-					
-					ser.community[giftedIndex].money += amount
-					ser.community[gifterIndex].money -= amount
-					
-					cache[ser.id] = ser
-					database.write(ser.id, ser)
-				} else {
-					respond {
-						allowedMentions()
-						content = "You don't have enough money in your bank to perform this action"
-					}
-				}
-			}
-		}
 		/* Chat Commands */
 		// Info
 		chatCommand(::InfoArgs)
