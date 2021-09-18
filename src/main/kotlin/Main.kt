e69fd3c6a185adb6d8a09b5a0b091e535e1b7732
Unicorn7141 <harelibr@gmail.com>
2021-09-18 15:55:29 +0300

    Corrected Code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,6 +8,7 @@ import com.kotlindiscord.kord.extensions.checks.types.Check
 import com.kotlindiscord.kord.extensions.utils.scheduling.Scheduler
 import com.kotlindiscord.kord.extensions.utils.scheduling.Task
 import com.mongodb.client.MongoCollection
+import com.mongodb.client.MongoDatabase
 import dev.kord.common.Color
 import dev.kord.common.entity.Snowflake
 import dev.kord.core.entity.Member
@@ -27,40 +28,40 @@ import java.time.Instant
 val TOKEN = System.getenv("TOKEN") ?: error("Please add TOKEN to your environmental variables")
 val connectURI = System.getenv("CONNECT_URI") ?: error("Please add a mongodb uri to connect with your database")
 val dbclient = KMongo.createClient(connectURI)
-val db = dbclient.getDatabase("CommunityBot")
+val db: MongoDatabase = dbclient.getDatabase("CommunityBot")
 val database = db.getCollection<Server>("Community")
-const val DEFAULT_PREFIX = "!"
+const val DEFAULT_PREFIX = "?"
 
 data class Server(
-    val id: String,
-    var prefix: String = DEFAULT_PREFIX,
-    val community: MutableList<CommunityMember> = mutableListOf()
+	val id: String,
+	var prefix: String = DEFAULT_PREFIX,
+	val community: MutableList<CommunityMember> = mutableListOf()
 ) {
-    val shop = Shop(mutableListOf())
+	val shop = Shop(mutableListOf())
 }
 
 data class CommunityMember(
-    val id: String,
-    var money: Int = 100,
-    val items: MutableList<Item> = mutableListOf<Item>(),
-    var lastWork: Instant? = null
+	val id: String,
+	var money: Int = 100,
+	val items: MutableList<Item> = mutableListOf(),
+	var lastWork: Instant? = null
 )
 
 enum class Type(val type: String) {
-    Role("role"),
-    Item("item"),
-    All("all")
+	Role("role"),
+	Item("item"),
+	All("all")
 }
 
 data class Shop(val items: MutableList<Item>)
 data class Item(
-    val type: Type,
-    val id: String,
-    val name: String,
-    val description: String = "",
-    val price: Int = 100,
-    var isLimited: Boolean = false,
-    var limitCount: Int = 0
+	val type: Type,
+	val id: String,
+	val name: String,
+	val description: String = "",
+	val price: Int = 100,
+	var isLimited: Boolean = false,
+	var limitCount: Int = 0
 )
 
 val cache = mutableMapOf<String, Server>()
@@ -68,141 +69,138 @@ lateinit var scheduler: Task
 
 @PrivilegedIntent
 suspend fun main() {
-    val bot = ExtensibleBot(TOKEN) {
-        chatCommands {
-            defaultPrefix = "!"
-            invokeOnMention = true
-            enabled = true
-            prefix { defaultPrefix ->
-                cache[guildId?.asString]?.prefix ?: defaultPrefix
-            }
-        }
-
-        applicationCommands {
-            this.enabled = true
-            defaultGuild(503652829685088276)
-        }
-
-        intents {
-            +Intents.all
-        }
-
-        members {
-            fillPresences = true
-            all()
-        }
-
-        extensions {
-            help {
-                enableBundledExtension = true
-                colour {
-                    when (guildId) {
-                        null ->
-                            DISCORD_BLURPLE
-                        else -> message.getAuthorAsMember()?.color() ?: error("Couldn't get member from this message")
-                    }
-                }
-                pingInReply = false
-    
-                deleteInvocationOnPaginatorTimeout = !true
-                deletePaginatorOnTimeout = false
-            }
-            add(::CommunityCommands)
-            add(::ShopCommands)
-            add(::Tests)
-        }
-    
-        scheduler = Scheduler().schedule(60, name = "readDB") {
-            for ((key, _) in cache) {
-                cache[key] = database.findOne(Server::id eq key)!!
-            }
-            restart(scheduler)
-        }
-    }
-
-    // When bot is launched
-    bot.on<ReadyEvent> {
-        for (guild in guilds) {
-            val ser = try {
-                println("I know this guild: ${guild.id.asString}")
-                database.findOne { Server::id eq guild.id.asString } ?: error("Server not found in the database")
-            } catch (e: Exception) {
-                println("I added this guild: ${guild.id.asString}")
-                val members = guild.members.toList().filter { !it.isBot }
-                val cmembers = mutableListOf<CommunityMember>()
-                for (member in members) {
-                    with(member) {
-                        cmembers.add(CommunityMember(id.asString))
-                    }
-                }
-                Server(guild.id.asString, DEFAULT_PREFIX, cmembers)
-
-            }
-
-            try {
-                database.write(ser.id, ser)
-                cache[guild.id.asString] = ser
-            } catch (e: Exception) {
-                println(e.message ?: e.cause ?: e)
-            }
-        }
-    }
-
-    // When bot is added to a new guild
-    bot.on<GuildCreateEvent> {
-        try {
-            database.findOne(Server::id eq guild.id.asString)
-        } catch (e: Exception) {
-            println("Added to a new guild: ${guild.id.asString}")
-            val members = guild.members.toList().filter { !it.isBot }
-            val cmembers = mutableListOf<CommunityMember>()
-            for (member in members) {
-                with(member) {
-                    cmembers.add(CommunityMember(id.asString, 0))
-                }
-            }
-            val ser = Server(guild.id.asString, DEFAULT_PREFIX, cmembers)
-            cache[ser.id] = ser
-            database.write(ser.id, ser)
-        }
-    }
-    // When bot is removed from a guild
-    bot.on<GuildDeleteEvent> {
-        println("Removed from guild: ${guild!!.id.asString}")
-        val ser = cache[guildId.asString] ?: error("Guild not found")
-        database.deleteOne(Server::id eq ser.id)
-        cache.remove(ser.id)
-    }
-
-    // When a new member joins the guild
-    bot.on<MemberJoinEvent> {
-        val ser = cache[guildId.asString] ?: error("Guild not found")
-        with(ser) {
-            if (!member.isBot)
-                community.add(CommunityMember(member.id.asString))
-        }
-        cache[ser.id] = ser
-        database.write(ser.id, ser)
-    }
-    // When a member leaves the guild
-    bot.on<MemberLeaveEvent> {
-        val ser = cache[guildId.asString] ?: error("Guild not found")
-        ser.community.removeIf { it.id == user.id.asString }
-        cache[ser.id] = ser
-        database.write(ser.id, ser)
-    }
-
-
-
-
-    bot.start()
+	val bot = ExtensibleBot(TOKEN) {
+		chatCommands {
+			defaultPrefix = "!"
+			invokeOnMention = true
+			enabled = true
+			prefix { defaultPrefix ->
+				cache[guildId?.asString]?.prefix ?: defaultPrefix
+			}
+		}
+		
+		applicationCommands {
+			this.enabled = true
+			defaultGuild(503652829685088276)
+		}
+		
+		intents {
+			+Intents.all
+		}
+		
+		members {
+			fillPresences = true
+			all()
+		}
+		
+		extensions {
+			help {
+				enableBundledExtension = true
+				colour {
+					when (guildId) {
+						null ->
+							DISCORD_BLURPLE
+						else -> message.getAuthorAsMember()?.color() ?: error("Couldn't get member from this message")
+					}
+				}
+				pingInReply = false
+				
+				deleteInvocationOnPaginatorTimeout = !true
+				deletePaginatorOnTimeout = false
+			}
+			add(::CommunityCommands)
+			add(::ShopCommands)
+			add(::Tests)
+		}
+		
+		scheduler = Scheduler().schedule(60, name = "readDB") {
+			for ((key, _) in cache) {
+				cache[key] = database.findOne(Server::id eq key)!!
+			}
+			restart(scheduler)
+		}
+	}
+// When bot is launched
+	bot.on<ReadyEvent> {
+		for (guild in guilds) {
+			val ser = try {
+				println("I know this guild: ${guild.id.asString}")
+				database.findOne { Server::id eq guild.id.asString } ?: error("Server not found in the database")
+			} catch (e: Exception) {
+				println("I added this guild: ${guild.id.asString}")
+				val members = guild.members.toList().filter { !it.isBot }
+				val cmembers = mutableListOf<CommunityMember>()
+				for (member in members) {
+					with(member) {
+						cmembers.add(CommunityMember(id.asString))
+					}
+				}
+				Server(guild.id.asString, DEFAULT_PREFIX, cmembers)
+				
+			}
+			
+			try {
+				database.write(ser.id, ser)
+				cache[guild.id.asString] = ser
+			} catch (e: Exception) {
+				println(e.message ?: e.cause ?: e)
+			}
+		}
+	}
+// When bot is added to a new guild
+	bot.on<GuildCreateEvent> {
+		try {
+			database.findOne(Server::id eq guild.id.asString)
+		} catch (e: Exception) {
+			println("Added to a new guild: ${guild.id.asString}")
+			val members = guild.members.toList().filter { !it.isBot }
+			val cmembers = mutableListOf<CommunityMember>()
+			for (member in members) {
+				with(member) {
+					cmembers.add(CommunityMember(id.asString, 0))
+				}
+			}
+			val ser = Server(guild.id.asString, DEFAULT_PREFIX, cmembers)
+			cache[ser.id] = ser
+			database.write(ser.id, ser)
+		}
+	}
+// When bot is removed from a guild
+	bot.on<GuildDeleteEvent> {
+		println("Removed from guild: ${guild!!.id.asString}")
+		val ser = cache[guildId.asString] ?: error("Guild not found")
+		database.deleteOne(Server::id eq ser.id)
+		cache.remove(ser.id)
+	}
+// When a new member joins the guild
+	bot.on<MemberJoinEvent> {
+		val ser = cache[guildId.asString] ?: error("Guild not found")
+		with(ser) {
+			if (!member.isBot)
+				community.add(CommunityMember(member.id.asString))
+		}
+		cache[ser.id] = ser
+		database.write(ser.id, ser)
+	}
+// When a member leaves the guild
+	bot.on<MemberLeaveEvent> {
+		val ser = cache[guildId.asString] ?: error("Guild not found")
+		ser.community.removeIf { it.id == user.id.asString }
+		cache[ser.id] = ser
+		database.write(ser.id, ser)
+	}
+	
+	
+	
+	
+	bot.start()
 }
 
 /**
  * Returns the color of a member (defaults to blurple)
  */
 suspend fun Member.color(): Color {
-    return roles.sorted().toList().reversed().firstOrNull { it.color.rgb != 0 }?.color ?: Color(7506394)
+	return roles.sorted().toList().reversed().firstOrNull { it.color.rgb != 0 }?.color ?: Color(7506394)
 }
 
 /**
@@ -213,12 +211,12 @@ suspend fun Member.color(): Color {
  * [ser] -> The new server
  */
 internal fun <TDocument> MongoCollection<TDocument>.write(id: String, ser: Server) {
-    val serv = database.findOne { Server::id eq id }
-    if (serv == null) {
-        database.insertOne(ser)
-    } else {
-        database.updateOne(Server::id eq id, ser)
-    }
+	val serv = database.findOne { Server::id eq id }
+	if (serv == null) {
+		database.insertOne(ser)
+	} else {
+		database.updateOne(Server::id eq id, ser)
+	}
 }
 
 /**
@@ -227,29 +225,28 @@ internal fun <TDocument> MongoCollection<TDocument>.write(id: String, ser: Serve
  * [memberId] -> The member's ID
  */
 fun botOwner(memberId: Long): Check<*> = {
-    val logger = KotlinLogging.logger("com.kotlindiscord.kord.extensions.checks.botOwner")
-    val member = memberFor(event)
-    
-    if (member == null) {
-        logger.nullMember(event)
-        pass()
-        
-    } else {
-        val memberObj = member.asMember()
-
-        val result = when (memberObj.id) {
-            Snowflake(memberId) -> true
-            else -> false
-        }
-
-        if (result) {
-            logger.failed("Member $member is the bot owner")
-
-        } else {
-            logger.passed()
-            pass()
-        }
-    }
+	val logger = KotlinLogging.logger("com.kotlindiscord.kord.extensions.checks.botOwner")
+	val member = memberFor(event)
+	
+	if (member == null) {
+		logger.nullMember(event)
+		pass()
+		
+	} else {
+		val memberObj = member.asMember()
+		val result = when (memberObj.id) {
+			Snowflake(memberId) -> true
+			else -> false
+		}
+		
+		if (result) {
+			logger.failed("Member $member is the bot owner")
+			
+		} else {
+			logger.passed()
+			pass()
+		}
+	}
 }
 
 /**
@@ -258,29 +255,28 @@ fun botOwner(memberId: Long): Check<*> = {
  * [memberId] -> The member's ID
  */
 fun botOwner(memberId: String): Check<*> = {
-    val logger = KotlinLogging.logger("com.kotlindiscord.kord.extensions.checks.botOwner")
-    val member = memberFor(event)
-    
-    if (member == null) {
-        logger.nullMember(event)
-        pass()
-        
-    } else {
-        val memberObj = member.asMember()
-
-        val result = when (memberObj.id) {
-            Snowflake(memberId) -> true
-            else -> false
-        }
-
-        if (result) {
-            logger.failed("Member $member is the bot owner")
-
-        } else {
-            logger.passed()
-            pass()
-        }
-    }
+	val logger = KotlinLogging.logger("com.kotlindiscord.kord.extensions.checks.botOwner")
+	val member = memberFor(event)
+	
+	if (member == null) {
+		logger.nullMember(event)
+		pass()
+		
+	} else {
+		val memberObj = member.asMember()
+		val result = when (memberObj.id) {
+			Snowflake(memberId) -> true
+			else -> false
+		}
+		
+		if (result) {
+			logger.failed("Member $member is the bot owner")
+			
+		} else {
+			logger.passed()
+			pass()
+		}
+	}
 }
 
 /**
@@ -289,29 +285,28 @@ fun botOwner(memberId: String): Check<*> = {
  * [memberId] -> The member's ID
  */
 fun botOwner(memberId: Snowflake): Check<*> = {
-    val logger = KotlinLogging.logger("com.kotlindiscord.kord.extensions.checks.botOwner")
-    val member = memberFor(event)
-    
-    if (member == null) {
-        logger.nullMember(event)
-        pass()
-        
-    } else {
-        val memberObj = member.asMember()
-
-        val result = when (memberObj.id) {
-            memberId -> true
-            else -> false
-        }
-
-        if (result) {
-            logger.failed("Member $member is the bot owner")
-
-        } else {
-            logger.passed()
-            pass()
-        }
-    }
+	val logger = KotlinLogging.logger("com.kotlindiscord.kord.extensions.checks.botOwner")
+	val member = memberFor(event)
+	
+	if (member == null) {
+		logger.nullMember(event)
+		pass()
+		
+	} else {
+		val memberObj = member.asMember()
+		val result = when (memberObj.id) {
+			memberId -> true
+			else -> false
+		}
+		
+		if (result) {
+			logger.failed("Member $member is the bot owner")
+			
+		} else {
+			logger.passed()
+			pass()
+		}
+	}
 }
 
 /**
@@ -320,5 +315,5 @@ fun botOwner(memberId: Snowflake): Check<*> = {
  * [scheduler] -> The task you'd like to restart
  */
 fun restart(task: Task) {
-    task.restart()
+	task.restart()
 }
